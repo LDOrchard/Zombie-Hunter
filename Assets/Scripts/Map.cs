d9cdaf6eab774b8bb49cd2fd825007a670b082b9
LDOrchard <43548000+LDOrchard@users.noreply.github.com>
2019-10-21 15:53:20 +0100

    Rock Prefab and Map Script(wip)
    Made a rock prefab with circle colision and a work in progress Map Script

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public Camera PlayCam;
+    public Camera MapCam;
+
+    void Start()
+    {
+        PlayCam.enabled = PlayCam.enabled;
+        MapCam.enabled = !MapCam.enabled;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            //Swap enabled state to opposite one provided that only is on at a time
+            PlayCam.enabled = !PlayCam.enabled;
+            MapCam.enabled = !MapCam.enabled;
+        }
+    }
+}
